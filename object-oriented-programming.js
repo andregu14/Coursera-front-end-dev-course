87e4a042f7424f510747bc4dbac0307576e77bed
André Gustavo <andregustavo147@gmail.com>
2023-06-27 02:45:00 -0300

    Create object-oriented-programming.js

---
+++ object-oriented-programming.js
@@ -0,0 +1,42 @@
+// // OOP 1
+// var purchase1 = {
+//     shoes: 100,
+//     stateTax: 1.2,
+//     totalPrice: function() {
+//         var calculation = purchase1.shoes * purchase1.stateTax;
+//         console.log('Total price:', calculation);
+//     }
+// }
+// purchase1.totalPrice() // 120
+
+// // OOP 2
+// var purchase2 = {
+//     shoes: 50,
+//     stateTax: 1.2,
+//     totalPrice: function() {
+//         var calculation = purchase2.shoes * purchase2.stateTax;
+//         console.log('Total price:', calculation);
+//     }
+// }
+// purchase2.totalPrice() // 60
+
+//OOP 3 // Using this
+var purchase1 = {
+    shoes: 100,
+    stateTax: 1.2,
+    totalPrice: function() {
+        var calculation = this.shoes * this.stateTax; // Using this
+        console.log('Total price:', calculation);
+    }
+}
+purchase1.totalPrice() // 120
+
+var purchase2 = {
+    shoes: 50,
+    stateTax: 1.2,
+    totalPrice: function() {
+        var calculation = this.shoes * this.stateTax; // Using this
+        console.log('Total price:', calculation);
+    }
+}
+purchase2.totalPrice() // 60
