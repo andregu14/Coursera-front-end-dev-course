d69d381f2f8a4122c020fddbd54f87121f3ed46d
André Gustavo <andregustavo147@gmail.com>
2023-06-26 21:13:58 -0300

    Create arrays-and-objects.js

---
+++ lesson005/arrays-and-objects.js
@@ -0,0 +1,10 @@
+var clothes = []
+clothes.push('shirt', 'pants', 'hood', 'shoes', 'underwear')
+clothes.pop()
+clothes.push('gloves')
+console.log(clothes[2])
+
+var favCar = {}
+favCar.color = 'blue'
+favCar.convertible = false
+console.log(favCar);
