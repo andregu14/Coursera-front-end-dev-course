881d8c1d0fe58a33d30048eb814b10f47a1fb5be
André Gustavo <andregustavo147@gmail.com>
2023-07-04 22:51:00 -0300

    Create ModeToggler component to test react events syntax

--- lesson008 react/customizing-example/src/App.js
+++ lesson008 react/customizing-example/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Btn from './components/Btn';
+import ModeToggler from './components/ModeToggler';
 
 function App() {
   return (
-    <Btn />
+    <ModeToggler />
   );
 }
 
