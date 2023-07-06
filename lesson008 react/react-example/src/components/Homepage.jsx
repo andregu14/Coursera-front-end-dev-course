bae30b649a8dfce2ae560428aee9d5c16e2dc13b
André Gustavo <andregustavo147@gmail.com>
2023-07-06 02:23:53 -0300

    Create Homepage.jsx

---
+++ lesson008 react/react-example/src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import AddImageExample from "./AddImageExample"
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <AddImageExample />
+        </div>
+    )
+}
+
+export default Homepage
