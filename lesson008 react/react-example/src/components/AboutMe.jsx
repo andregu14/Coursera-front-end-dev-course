946d25e6ac98d58b56816a15361c427df8b0d1dc
André Gustavo <andregustavo147@gmail.com>
2023-07-06 02:23:51 -0300

    Create AboutMe.jsx

---
+++ lesson008 react/react-example/src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import ReactPlayerExample from "./ReactPlayerExample"
+
+function AboutMe() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <ReactPlayerExample />
+        </div>
+    )
+}
+
+export default AboutMe
