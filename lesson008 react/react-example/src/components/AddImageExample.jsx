5a45c456ca5ec49e3738171f5d7fc16f6651684f
André Gustavo <andregustavo147@gmail.com>
2023-07-06 02:23:44 -0300

    Create AddImageExample component

---
+++ lesson008 react/react-example/src/components/AddImageExample.jsx
@@ -0,0 +1,12 @@
+import coxinha from "../assets/images/coxinha.jpg"
+
+function AddImageExample() {
+    return (
+        <div>
+            <h1>Adding Images on React</h1>
+            <img src={coxinha} alt="coxinha" />
+        </div>
+    )
+}
+
+export default AddImageExample
