80018a9b4656204afe37042b93f5f89221f17836
André Gustavo <andregustavo147@gmail.com>
2023-07-07 21:09:39 -0300

    Update Nav.jsx

--- lesson009 advanced react/advanced-react/src/components/Nav.jsx
+++ lesson009 advanced react/advanced-react/src/components/Nav.jsx
@@ -5,6 +5,7 @@ function Nav() {
     <nav>
         <Link to="/" className='nav-item'>Homepage</Link>
         <Link to="/desserts" className='nav-item'>Desserts</Link>
+        <Link to="/feedback" className="nav-item">Feedback</Link>
     </nav>
     )
 }
