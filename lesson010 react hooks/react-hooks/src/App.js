8858909cfa56afbf696001963249fe853e839381
André Gustavo <andregustavo147@gmail.com>
2023-07-09 00:04:40 -0300

    Create new React-Project

---
+++ lesson010 react hooks/react-hooks/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UseStateEx from './components/UseStateEx';
+import StateObject from './components/StateObject';
+
+function App() {
+  return (
+    <div className="App">
+      <UseStateEx />
+      <StateObject />
+    </div>
+  );
+}
+
+export default App;
