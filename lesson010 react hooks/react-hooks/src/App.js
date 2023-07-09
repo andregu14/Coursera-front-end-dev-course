4404b3ce80aabaf9860b51c1a94c3e0294780759
André Gustavo <andregustavo147@gmail.com>
2023-07-09 02:56:56 -0300

    Update App.js and index.js
    Adicionei sistema SPA

--- lesson010 react hooks/react-hooks/src/App.js
+++ lesson010 react hooks/react-hooks/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import UseStateEx from './components/UseStateEx';
-import StateObject from './components/StateObject';
+import Nav from './components/Nav';
+import { Route, Routes } from 'react-router-dom';
+import Goals from './components/Goals';
+import UsingUseEffect from './components/UsingUseEffect';
+
 
 function App() {
   return (
     <div className="App">
-      <UseStateEx />
-      <StateObject />
+      <Nav />
+
+      <Routes>
+        <Route path='/' element={<Goals />} />
+        <Route path='/use-state-example' element={<UseStateEx />} />
+        <Route path='/using-use-effect' element={<UsingUseEffect />} />
+      </Routes>
     </div>
   );
 }
