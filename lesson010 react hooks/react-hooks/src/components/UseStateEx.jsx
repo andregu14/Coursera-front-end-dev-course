e0665469501fc1282d400ac597fb053dbfb32004
André Gustavo <andregustavo147@gmail.com>
2023-07-09 00:04:46 -0300

    Create UseStateEx.jsx

---
+++ lesson010 react hooks/react-hooks/src/components/UseStateEx.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+const UseStateEx = () => {
+    const [randomNumber, setRandomNumber] = useState()
+    let random = () => {
+       return Math.round(Math.random() * 100)
+    }
+
+    return (
+        <>
+            <h1>Use State Hook</h1>
+            <p>Random number from 0 to 100 = <strong>{randomNumber}</strong></p>
+            <button onClick={ () => setRandomNumber(random)}>Click on the button to generate a random number</button>
+        </>
+     
+    )
+}
+
+export default UseStateEx
