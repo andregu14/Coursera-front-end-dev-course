04c122080c63c557879efa6a0f310af0c0431dd6
André Gustavo <andregustavo147@gmail.com>
2023-07-09 02:55:39 -0300

    Create Nav.jsx component

---
+++ lesson010 react hooks/react-hooks/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Nav() {
+    return (
+    <nav>
+        <Link to="/" className='nav-item'>Home</Link>
+        <Link to="/use-state-example" className='nav-item'>Use State Example</Link>
+        <Link to="/using-use-effect" className='nav-item'>Using Use Effect Example</Link>
+    </nav>
+    )
+}
+
+export default Nav
