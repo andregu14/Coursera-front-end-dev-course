4fbc10d6c89c55fc537daa5c77daf5598150d988
André Gustavo <andregustavo147@gmail.com>
2023-07-10 02:06:36 -0300

    Create HOCExample.jsx

---
+++ lesson010 react hooks/react-hooks/src/components/HOCExample.jsx
@@ -0,0 +1,72 @@
+const { useState, useEffect } = require("react")
+
+const withMousePosition = (WrappedComponent) => {
+    return (props) => {
+        const [mousePosition, setMousePosition] = useState({
+            x: 0,
+            y: 0,        
+        })
+
+        useEffect(() => {
+            const handleMousePositionChange = (e) => {
+                setMousePosition({
+                    x: e.clientX,
+                    y: e.clientY,
+                })
+            }
+
+            window.addEventListener("mousemove", handleMousePositionChange)
+
+            return () => {
+                window.removeEventListener("mousemove", handleMousePositionChange)
+            }
+        }, [])
+
+
+        return <WrappedComponent {...props} mousePosition={mousePosition} />
+            
+    }
+}
+
+
+const PanelMouseLogger = ({mousePosition}) => {
+    if (!mousePosition) {
+        return null
+    }
+    
+    return (
+        <div className="BasicTracker">
+            <p>Mouse position:</p>
+            <div className="Row">
+                <span>x: {mousePosition.x}</span>
+                <span>y: {mousePosition.y}</span>
+            </div>
+        </div>
+    )
+}
+
+const PointMouseLogger = ({mousePosition}) => {
+    if (!mousePosition) {
+        return null
+    }
+    return (
+        <p>
+            ({mousePosition.x}, {mousePosition.y})
+        </p>
+    )
+}
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger)
+const PointMouseTracker = withMousePosition(PointMouseLogger)
+
+function HOCExample() {
+    return (
+        <div>
+            <h2>Using HOC</h2>
+            <PanelMouseTracker />
+            <PointMouseTracker />
+        </div>
+    )
+}
+
+export default HOCExample
