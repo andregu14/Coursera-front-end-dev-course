9cb7dc83b3d43e002e73816fe9e8e39aeb478636
André Gustavo <andregustavo147@gmail.com>
2023-07-10 01:41:11 -0300

    Update FetchingData.jsx

--- lesson010 react hooks/react-hooks/src/components/FetchingData.jsx
+++ lesson010 react hooks/react-hooks/src/components/FetchingData.jsx
@@ -18,7 +18,7 @@ export default function FetchingData() {
             <h1>Data returned</h1>
             <h2>First Name: {user.results[0].name.first}</h2>
             <h2>Last Name: {user.results[0].name.last}</h2>
-            <img src={user.results[0].picture.large} alt="User profile image" />
+            <img src={user.results[0].picture.large} alt="User profile" />
         </div>
     ) : (
         <h1>Data pending...</h1>
